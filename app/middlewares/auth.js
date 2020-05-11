ec03cf52829629ce888a7e2c3a202e8a21a6f387
Douglas Tofoli <contato@dougrt.dev>
2020-05-11 16:49:29 -0300

    Create auth middleware to autenticate user

---
+++ app/middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config
+const { JWT_TOKEN } = process.env;
+
+const User = require('../models/user');
+
+const withAuth = (req, res, next) => {
+  const token = req.headers['x-access-token'];
+
+  if (!token) {
+    res.status(401).json({ error: 'Unauthorized: no token provided' });
+  } else {
+    jwt.verify(token, JWT_TOKEN, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ error: 'Unauthorized: token invalid' });
+      } else {
+        // req.email = decoded.email;
+        User.findOne({ email: decoded.email })
+        .then(user => {
+          req.user = user;
+          next();
+        })
+        .catch(err => {
+          res.status(401).json({ error: err });
+        })
+      }
+    });
+  }
+}
+
+module.exports = withAuth;

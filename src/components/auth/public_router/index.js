4cc3e7c9f2f4d53d2a0e2cd61a121c661b565cd8
Douglas Tofoli <contato@dougrt.dev>
2020-05-13 13:31:31 -0300

    Components Form for pages login and register

---
+++ src/components/auth/public_router/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const publicRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => ( 
+    localStorage.getItem('user')
+    ? <Redirect to={{ pathname: '/notes' }} />
+    : <Component {...props} />
+  )} />
+);
+
+export default publicRoute;

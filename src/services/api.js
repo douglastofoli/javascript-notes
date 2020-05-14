a3b6c33a7319831de96a4d7dcb0fcdf86e17ca50
Douglas Tofoli <contato@dougrt.dev>
2020-05-14 18:48:23 -0300

    Deploy app

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const Api = axios.create({ baseURL: 'http://localhost:3030' });
+const Api = axios.create({ baseURL: 'http://192.168.1.254:3001' });
 
 export default Api;

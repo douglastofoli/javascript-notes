4ef35be5b27452fdf24928cb8e09e35751e54856
Douglas Tofoli <contato@dougrt.dev>
2020-05-14 01:30:28 -0300

    Connection to API

--- src/services/notes.js
+++ src/services/notes.js
@@ -4,7 +4,7 @@ const NotesService = {
   index: () => Api.get('/notes', {
     headers: {'x-access-token': localStorage.getItem('token') }
   }),
-  create: () => Api.post('/notes', {'title': 'Nova nota', 'body': 'Nova nota...' }, {
+  create: () => Api.post('/notes', { 'title': 'Nova nota', 'body': 'Nova nota...' }, {
     headers: {'x-access-token': localStorage.getItem('token') }
   }),
   delete: (id) => Api.delete(`/notes/${id}`, {
